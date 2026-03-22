bab17eda906c044669ec2b69920c0e8e95f0cefb
saeed034666 <saeedsikander032@gmail.com>
2026-03-22 16:40:30 +0500

    Add files via upload

---
+++ Lab 2/Lab work/LAB_2 LabWork_3-CPP.cpp
@@ -0,0 +1,22 @@
+// Name: Saeed Ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0718
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;  // declare variables
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;  // take input from user
+
+    // Conversion formula
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
+
