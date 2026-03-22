c507d4e73ee8a1934f9a34c2f70f9885cec9761c
saeed034666 <saeedsikander032@gmail.com>
2026-03-22 15:59:36 +0500

    Add files via upload

---
+++ Lab 1/Lab Work/LAB_01 LabWork_1-CPP.cpp
@@ -0,0 +1,17 @@
+// Name: Saeed Ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0718
+// Section: A
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
