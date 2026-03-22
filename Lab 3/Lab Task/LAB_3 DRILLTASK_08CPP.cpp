04fa8fa827294f001a30654c368eab19e5a5c1ee
saeed034666 <saeedsikander032@gmail.com>
2026-03-22 17:03:28 +0500

    Add files via upload

---
+++ Lab 3/Lab Task/LAB_3 DRILLTASK_08CPP.cpp
@@ -0,0 +1,33 @@
+// Name: saeed ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0718
+// Section: A
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float a, b, c, root1, root2, D;
+
+    cout << "Enter coefficient a: ";
+    cin >> a;
+
+    cout << "Enter coefficient b: ";
+    cin >> b;
+
+    cout << "Enter coefficient c: ";
+    cin >> c;
+
+    D = b*b - 4*a*c;
+
+    root1 = (-b + sqrt(D)) / (2*a);
+    root2 = (-b - sqrt(D)) / (2*a);
+
+    cout << "Root 1 = " << root1 << endl;
+    cout << "Root 2 = " << root2 << endl;
+
+    return 0;
+}
+
+
---
+++ Lab 3/Lab Task/Lab 3 Task 1-CPP.cpp
@@ -0,0 +1,24 @@
+// Name: Saeed ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0718
+// Section: A
+#include <iostream>
+using namespace std;
+
+int main() {
+    double voltage, current, resistance;
+
+    cout << "Enter voltage (V): ";
+    cin >> voltage;
+
+    cout << "Enter current (I): ";
+    cin >> current;
+
+    resistance = voltage / current;
+
+    cout << "Voltage: " << voltage << " V" << endl;
+    cout << "Current: " << current << " A" << endl;
+    cout << "Resistance: " << resistance << " Ohms" << endl;
+
+    return 0;
+}
