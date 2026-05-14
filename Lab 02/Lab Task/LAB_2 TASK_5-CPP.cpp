6c3179e0b683a201ccdfeac955746ff265bb346c
Saeed ullah <saeedsikander032@gmail.com>
2026-05-15 00:39:06 +0500

    Add files via upload

---
+++ Lab 02/Lab Task/LAB_2 TASK_5-CPP.cpp
@@ -0,0 +1,24 @@
+// Name: Saeed ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0718
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // printing name 10 times
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+    cout << "saeed" << endl;
+
+    return 0;  // program ends
+}
